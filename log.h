e79e7f3d326b962cd7883096349ecb6da4afea7b
Sam Harada <sam@antumbra.io>
2014-08-05 21:32:08 -0400

    Document log fns.

--- log.h
+++ log.h
@@ -6,11 +6,14 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+/* Log message with file/line/func context. */
 #define An_LOG(ctx, fmt, ...) AnLog_Log((ctx), ("[%s:%d:%s] " fmt "\n"), __FILE__, \
                                         __LINE__, __func__, __VA_ARGS__)
 
+/* Log message. */
 void AnLog_Log(AnCtx *ctx, const char *fmt, ...);
 
+/* Set output file for logging, or NULL to disable. */
 void AnLog_SetLogFile(AnCtx *ctx, FILE *f);
 
 #endif
