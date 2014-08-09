2121ccd525402d7a374c093c54c4a6d3fcddd70f
Sam Harada <sam@antumbra.io>
2014-08-09 02:16:10 -0400

    Implement sweep test.

--- test.c
+++ test.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <time.h>
+#include <math.h>
+#include <string.h>
 
 #include "antumbra.h"
+
+#include "hsv.c"
 
 int main(int argc, char **argv)
 {
@@ -20,6 +27,26 @@ int main(int argc, char **argv)
             fputs("device open failed\n", stderr);
             return 1;
         }
+
+        uint8_t col[3];
+        float deg = 0;
+
+        uint8_t outb[8];
+        memset(outb, 0, sizeof outb);
+
+        while (1) {
+            hsv2rgb(deg, 1, 1, col, col + 1, col + 2);
+            deg = fmodf(deg + 1, 360);
+
+            memcpy(outb, col, sizeof col);
+            if (AnDevice_SendBulkPacket_S(ctx, dev, sizeof outb, outb) ==
+                AnError_DISCONNECTED)
+                return 1;
+
+            struct timespec ts = {.tv_sec = 0, .tv_nsec = 10000000};
+            nanosleep(&ts, NULL);
+        }
+
         AnDevice_Close(ctx, dev);
         AnDevice_Free(dev);
     }
