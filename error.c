204c5393a156beb7030d1880661b4d91bc2521b3
Sam Harada <sam@antumbra.io>
2014-08-25 21:11:35 -0400

    Define messages for WRONGSTATE and OUTOFRANGE.

--- error.c
+++ error.c
@@ -7,6 +7,8 @@ static const char *err_strings[] = {
     [AnError_DISCONNECTED] = "Disconnected",
     [AnError_MALLOCFAILED] = "Memory allocation failed",
     [AnError_LIBUSB] = "libusb error",
+    [AnError_WRONGSTATE] = "Device in wrong state for operation",
+    [AnError_OUTOFRANGE] = "Value out of range",
 };
 
 const char *AnError_String(AnError e)
