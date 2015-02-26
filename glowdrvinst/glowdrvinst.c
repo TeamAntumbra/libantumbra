e6d0d8b4fa685b6122ec3dbae144cc64ade2291d
Sam Harada <sam@antumbra.io>
2015-02-26 01:37:04 -0500

    libwdi doesn't actually need a connected device.
    (oops)

--- glowdrvinst/glowdrvinst.c
+++ glowdrvinst/glowdrvinst.c
@@ -18,23 +18,53 @@ void die(int st)
     exit(st);
 }
 
-struct usbpair {
-    uint16_t vid;
-    uint16_t pid;
-    bool didinstall;
-};
-
-struct usbpair usbids[] = {
-    {0x16d0, 0x0a85, false},
-};
-
-struct usbpair *lookup_ids(uint16_t vid, uint16_t pid)
+void install_driver(uint16_t vid, uint16_t pid)
 {
-    for (int i = 0; i < sizeof usbids / sizeof *usbids; ++i) {
-        if (usbids[i].vid == vid && usbids[i].pid == pid)
-            return &usbids[i];
+    printf("Will install WinUSB driver for VID 0x%04x PID 0x%04x\n",
+           (unsigned int)vid, (unsigned int)pid);
+
+    struct wdi_options_prepare_driver prepopts = {
+        .driver_type = WDI_WINUSB,
+        .vendor_name = "Antumbra",
+        .device_guid = NULL,
+        .disable_cat = false,
+        .disable_signing = false,
+        .cert_subject = NULL,
+        .use_wcid_driver = false,
+    };
+    struct wdi_device_info dev = {
+        .next = NULL,
+        .vid = vid,
+        .pid = pid,
+        .is_composite = false,
+        .mi = 0,
+        .desc = "Glow",
+        .driver = NULL,
+        .device_id = NULL,
+        .hardware_id = NULL,
+        .compatible_id = NULL,
+        .upper_filter = NULL,
+        .driver_version = 0,
+    };
+    int err;
+
+    const char prefix[] = "glowinftmp";
+    char tmpdir[sizeof prefix + 8 + 1];
+    sprintf(tmpdir, "%s%04x%04x", prefix, (unsigned int)vid, (unsigned int)pid);
+
+    err = wdi_prepare_driver(&dev, tmpdir, "glow.inf", &prepopts);
+    if (err) {
+        fprintf(stderr, "wdi_prepare_driver: %s\n", wdi_strerror(err));
+        die(1);
     }
-    return NULL;
+
+    err = wdi_install_driver(&dev, tmpdir, "glow.inf", NULL);
+    if (err) {
+        fprintf(stderr, "wdi_install_driver: %s\n", wdi_strerror(err));
+        die(1);
+    }
+
+    fputs("Installation successful\n", stdout);
 }
 
 int main(int argc, char **argv)
@@ -47,79 +77,35 @@ int main(int argc, char **argv)
             elevated = true;
     }
 
-    struct wdi_device_info *list;
-    struct wdi_options_create_list listopts = {0};
-    struct wdi_options_prepare_driver prepopts = {0};
-    prepopts.driver_type = WDI_WINUSB;
-    prepopts.vendor_name = "Antumbra";
-
     fputs("Antumbra Glow WinUSB driver installer\n", stdout);
 
-    int err = wdi_create_list(&list, &listopts);
-    if (err && err != WDI_ERROR_NO_DEVICE) {
-        fprintf(stderr, "wdi_create_list: %s\n", wdi_strerror(err));
-        die(1);
-    }
-    if (err == WDI_ERROR_NO_DEVICE)
-        list = NULL;
+    if (!batch && !elevated) {
+        fputs("Trying to relaunch with elevated privileges\n", stdout);
 
-    bool didinstall = false;
-    for (struct wdi_device_info *dev = list; dev; dev = dev->next) {
-        printf("Found unassociated device: VID 0x%04x PID 0x%04x",
-               (unsigned int)dev->vid, (unsigned int)dev->pid);
-
-        struct usbpair *pair = lookup_ids(dev->vid, dev->pid);
-        if (!pair) {
-            fputs("Ignore non-Glow device", stdout);
-            continue;
+        char buf[MAX_PATH];
+        GetModuleFileName(NULL, buf, sizeof buf);
+        printf("Executable is: %s\n", buf);
+        int ret = ShellExecute(NULL, "runas", buf, "elevated", NULL, 5);
+        if (ret > 32) {
+            fputs("Successfully relaunched; exiting unprivileged instance.\n", stdout);
+            die(0);
         }
-        if (pair->didinstall) {
-            fputs("Already installed for this VID/PID", stdout);
-            continue;
-        }
-
-        /* Only try to elevate privileges if there is at least one device to
-           install. */
-        if (!batch && !elevated) {
-            fputs("Trying to relaunch with elevated privileges", stdout);
-
-            char buf[MAX_PATH];
-            GetModuleFileName(NULL, buf, sizeof buf);
-            printf("Executable is: %s\n", buf);
-            int ret = ShellExecute(NULL, "runas", buf, "elevated", NULL, 5);
-            die(ret > 32 ? 0 : 1);
-        }
-
-        static const char prefix[] = "glowinftmp";
-        char tmpdir[sizeof prefix + 8 + 1];
-        sprintf(tmpdir, "%s%04x%04x", prefix,
-                (unsigned int)dev->vid & 0xffff, (unsigned int)dev->pid & 0xffff);
-
-        err = wdi_prepare_driver(dev, tmpdir, "glow.inf", &prepopts);
-        if (err) {
-            fprintf(stderr, "wdi_prepare_driver: %s\n", wdi_strerror(err));
+        else {
+            fputs("Relaunch failed!\n", stdout);
             die(1);
         }
-
-        err = wdi_install_driver(dev, tmpdir, "glow.inf", NULL);
-        if (err) {
-            fprintf(stderr, "wdi_install_driver: %s\n", wdi_strerror(err));
-            die(1);
-        }
-
-        printf("Installed WinUSB driver for VID 0x%04x PID 0x%04x\n",
-               (unsigned int)dev->vid, (unsigned int)dev->pid);
-        pair->didinstall = true;
-        didinstall = true;
     }
 
-    if (!didinstall)
-        fputs("Did not install driver.\n"
-              "Either no Glow devices are connected, or all connected devices are already installed.\n"
-              "Note that driver installation requires at least one device to be connected.\n",
-              stdout);
+    if (!batch) {
+        fputs("Press ENTER to begin installation...\n", stdout);
+        for (int c; (c = getchar()) != '\n';) {
+            if (c == EOF)
+                exit(0);
+        }
+    }
 
-    wdi_destroy_list(list);
+    install_driver(0x16d0, 0x0a85);
+
     die(0);
     return 0;
 }
